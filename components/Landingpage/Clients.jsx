ff44d484a95b7c5d60b7831d9b537d89a215488e
manish kumar <manishkumarstudent8744@gmail.com>
2026-07-22 10:05:07 +0530

    client alt

--- components/Landingpage/Clients.jsx
+++ components/Landingpage/Clients.jsx
@@ -6,36 +6,38 @@ import Image from "next/image";
 import "swiper/css";
 
 export default function TrustedBrands() {
-const brands = [
-  "/client/24.png",
-  "/client/20.png",
-  "/client/19.png",
-  "/client/21.png",
-  "/client/22.png",
-  "/client/23.png",
-  "/client/1.png",
-  "/client/2.png",
-    "/client/3.png",
-      "/client/4.png",
-        "/client/5.jpeg",
-          "/client/6.jpeg",
-            "/client/7.png",
-              "/client/8.jpeg",
-                "/client/9.png",
-                  "/client/10.png",
-                    "/client/11.jpeg",
- "/client/12.png",
-  "/client/13.png",
-   "/client/14.png",
-    "/client/15.jpeg",
-     "/client/16.png",
-      "/client/17.png",
-       "/client/18.png",
-
+  const brands = [
+  { image: "/client/24.png", alt: "Adani" },
+  { image: "/client/20.png", alt: "Daikin" },
+  { image: "/client/19.png", alt: "Tata" },
+  { image: "/client/21.png", alt: "Maruti Suzuki" },
+  { image: "/client/22.png", alt: "Mahindra" },
+  { image: "/client/23.png", alt: "Hitachi" },
+  { image: "/client/1.png", alt: "Posh" },
+  { image: "/client/2.png", alt: "Ying Tong" },
+  { image: "/client/3.png", alt: "Boyd" },
+  { image: "/client/4.png", alt: "Ganesh" },
+  { image: "/client/5.jpeg", alt: "Intech Foundation" },
+  { image: "/client/6.jpeg", alt: "Star Plus Battery" },
+  { image: "/client/7.png", alt: "Tixo" },
+  { image: "/client/8.jpeg", alt: "Aashirdvad Agro Industries" },
+  { image: "/client/9.png", alt: "Kirloskar" },
+  { image: "/client/10.png", alt: "Sullair" },
+  { image: "/client/11.jpeg", alt: "MV Electrosystem pvt ltd" },
+  { image: "/client/12.png", alt: "Progress Rail" },
+  { image: "/client/13.png", alt: "Titagarh" },
+  { image: "/client/14.png", alt: "Estilo Sports" },
+  { image: "/client/15.jpeg", alt: "Indian Oil" },
+  { image: "/client/16.png", alt: "Shubham Pack" },
+  { image: "/client/17.png", alt: "Alutech" },
+  { image: "/client/18.png", alt: "Construction" },
 ];
 
   return (
-    <section id="industries"  className="relative py-6 md:py-10 bg-white border-y border-cyan-200 overflow-hidden">
+    <section
+      id="industries"
+      className="relative py-6 md:py-10 bg-white border-y border-cyan-200 overflow-hidden"
+    >
       <div className="w-full px-10 mx-auto ">
         <h3 className="text-center text-blue-950 font-bold tracking-[0.25em] text-sm mb-5 md:mb-10">
           TRUSTED BY LEADING INDUSTRIES
@@ -69,10 +71,10 @@ const brands = [
             },
           }}
         >
-       {brands.map((logo, index) => (
-  <SwiperSlide key={index}>
-    <div
-      className="
+          {brands.map((logo, index) => (
+            <SwiperSlide key={index}>
+              <div
+                className="
       h-24
       rounded-2xl
       border
@@ -87,13 +89,13 @@ const brands = [
       hover:-translate-y-1
       hover:shadow-xl
     "
-    >
-     <Image
-  src={logo}
-  alt=""
-  width={200}
-  height={180}
-  className="
+              >
+                <Image
+                  src={logo.image}
+                  alt={logo.alt}
+                  width={200}
+                  height={180}
+                  className="
     h-23
     w-auto
     object-contain
@@ -102,14 +104,12 @@ const brands = [
     duration-500
  
   "
-/>
-    </div>
-  </SwiperSlide>
-))}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
-
-
     </section>
   );
 }
