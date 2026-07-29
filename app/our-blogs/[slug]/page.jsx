e98691444743d168a064772536ee90a6acfab3dc
manish kumar <mk7219630@gmail.com>
2026-07-29 11:37:52 +0530

    blogs page changes

--- app/our-blogs/[slug]/page.jsx
+++ app/our-blogs/[slug]/page.jsx
@@ -103,7 +103,7 @@ export default async function BlogDetail({ params }) {
           <div className="text-cyan-500 max-w-6xl opacity-80 bg-white p-2 rounded">
        
 
-            <h1 className="text-xl md:text-4xl text-center font-bold leading-tight mt-15">
+            <h1 className="text-xl md:text-4xl text-center font-bold leading-tight">
               {blog.title}
             </h1>
 
